1ae70f726be865540efcf5a2848bd87a17cf44a0
Scott Anderson <scott.anderson@scandiweb.com>
2022-04-21 13:53:15 +0300

    main
    	- Add base plugin skeleton

---
+++ SwagAllEntities/src/SwagAllEntities.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ *  Swag AllEntities module
+ *
+ * @category  API
+ * @package   \Swag\AllEntities\SwagAllEntities
+ * @author    Scott Anderson <tri.s.anderson@shopware.com>
+ * @copyright 2022 ForBetterFuture
+ */
+namespace Swag\AllEntities;
+
+use Shopware\Core\Framework\Plugin;
+
+/**
+ * Class SwagAllEntities
+ */
+class SwagAllEntities extends Plugin
+{
+
+}
